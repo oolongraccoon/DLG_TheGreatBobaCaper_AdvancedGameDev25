f187fe68b81fceacd999b6b0959f5a35530f1303
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2025-04-05 04:44:40 +0300

    added double jump and gravity

--- Assets/scripts/Player/PlayerMovement.cs
+++ Assets/scripts/Player/PlayerMovement.cs
@@ -11,18 +11,38 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jumping")]
     public float jumpPower = 10f;
+    public int maxJumps = 2; //double jump
+    int jumpRemaining;
 
     [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
 
+    [Header("Gravity")]
+    public float baseGravity = 1f;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f;
+
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        GroundCheck();
+        Gravity();
     }
-
+    private void Gravity()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier; // fall increasingly faster
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
@@ -30,26 +50,29 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (isGrounded())
+        if (jumpRemaining > 0)
         {
             if (context.performed)
             {   //big jump
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                jumpRemaining--;
             }
             else if (context.canceled)
             {
                 //light jump
                 rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+                jumpRemaining--;
             }
         }
+
     }
-    private bool isGrounded()
+    private void GroundCheck()
     {
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
-            return true;
+            jumpRemaining = maxJumps;
         }
-        return false;
+ 
     }
     private void OnDrawGizmosSelected()
     {
