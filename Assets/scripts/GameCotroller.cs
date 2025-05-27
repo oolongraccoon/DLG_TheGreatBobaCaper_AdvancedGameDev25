71c5599221f8748abe6ced2d473f3073a29e52f8
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2025-05-27 20:10:21 +0300

    add the load level function

---
+++ Assets/scripts/GameCotroller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Persisting;
+
+public class GameCotroller : MonoBehaviour
+{
+    public Inventory inventory;
+    int progressAmount;
+ 
+    void Start()
+    {
+        progressAmount = 0;
+        Boba.OnBobaCollect += IncreaseProgressAmount;
+        HoldToLoadLevel.OnHoldComplete += LoadNextLevel;
+    }
+    void IncreaseProgressAmount(int amount)
+    {
+        progressAmount += amount;
+        if (progressAmount >= 4)
+        {
+            Debug.Log("level COMPLETE");
+        }
+
+    }
+    void LoadNextLevel()
+    {
+        LevelManagerPersist.instance.LoadLevel("SecondScene");
+    }
+
+}
--- Assets/scripts/Inventory/Boba.cs
+++ Assets/scripts/Inventory/Boba.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using System;
 
 public class Boba : MonoBehaviour, IItem
 {
     public ItemData itemData;
+    public static event Action<int> OnBobaCollect;
+    public int worth = 1;
 
     public ItemData Collect()
     {
         ItemData dataToReturn = itemData;
+
+        bool added = Inventory.instance.Add(itemData);
+        if (!added)
+        {
+            Debug.Log("Inventory is full! Item not collected.");
+            return null;
+        }
+
+        OnBobaCollect?.Invoke(worth);
         Destroy(gameObject); // object is safe to destroy AFTER data is saved
         return dataToReturn;
     }
