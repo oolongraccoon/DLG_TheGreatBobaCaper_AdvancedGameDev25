d41bd7c6a889745800190442514896329002ff64
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2025-06-02 06:20:01 +0300

    modify player initial location

--- Assets/scripts/UI/PersistentObject.cs
+++ Assets/scripts/UI/PersistentObject.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class PersistentObject : MonoBehaviour
 {
     private static PersistentObject instance;
+    public GameObject player;
 
     void Awake()
     {
@@ -20,6 +21,7 @@ public class PersistentObject : MonoBehaviour
             Destroy(gameObject); // Ensures only one instance exists
         }
     }
+
     // This method is called every time a new scene loads
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
@@ -28,7 +30,16 @@ public class PersistentObject : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        if (scene.name == "SecondScene")
+        {
+            player = GameObject.FindWithTag("Player"); // Assuming your player GameObject has the tag "Player"
+            if (player != null)
+            {
+                player.transform.position = new Vector3(-3f, 12f, 0f);//a fixed position in second scene
+            }
+        }
     }
+
 
     void OnDestroy()
     {
